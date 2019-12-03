95e10fbf1e0d1e066b526e2f4ac2074b9314b5b0
andy <andytan727@gmail.com>
2019-12-03 13:21:51 +0800

    bug fixed

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,19 +2,11 @@ import React from "react";
 const HomePage = () => {
   return (
     <>
-      <body>
-        <section className="intro">
-          <h1 className="title">
-            <span id="name">ENGAGE</span> better, with data.
-          </h1>
-        </section>
-
-        <section className="menu">
-          <button className="btnHome btn-green">WHAT</button>
-          <button className="btnHome btn-yellow">WHO </button>
-          <button className="btnHome btn-red">HOW</button>
-        </section>
-      </body>
+      <section className="intro">
+        <h1 className="title">
+          <span id="name">ENGAGE</span> better, with data.
+        </h1>
+      </section>
     </>
   );
 };
@@ -32,12 +24,11 @@ var names = ["TEACH", "PERFORM", "ENTERTAIN", "SELL", "PRESENT"];
 
 let num = 0;
 setInterval(function() {
-  if (num == names.length) {
+  if (num === names.length) {
     num = 0;
   } else {
     // var rand = Math.floor(Math.random() * names.length);
-    console.log(num);
-    document.getElementById("name").innerHTML = names[num];
+    // document.getElementById("name").innerHTML = names[num];
     num++;
   }
 }, 2500);
--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -1,14 +1,25 @@
 import React from "react";
 
 const MyProfile = props => {
-  const { username, id, email, name } = props.user;
+  const { username, email } = props.user;
   return (
     <>
-      <h1>MyProfile</h1>
-      <h1>{id}</h1>
-      <h1>{name}</h1>
-      <h1>{email}</h1>
-      <h1>{username}</h1>
+      <div className="container w-50">
+        <h1>MyProfile</h1>
+        <div className="row justify-content-center">
+          <div className="col-xl-5 col-xl-5 border">
+            <img
+              src="https://www.livelingua.com/img/profilesTeachers/103/Guillaume-Deneufbourg-Square_Profile_S.jpg"
+              className="myprofilepic"
+              alt="profile_pic"
+            />
+          </div>
+          <div className="col-xl-7 col-xl-7 border">
+            <h4>Username: {username}</h4>
+            <h4>Email: {email}</h4>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
