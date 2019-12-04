d4e02d5888bd6efea0a5762b076dec1faaaf771d
andy <andytan727@gmail.com>
2019-12-04 14:21:03 +0800

    fix validation

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,6 +1,18 @@
-import React, {useState} from 'react'
-import {Container, Row, Col, Card, CardBody, Media, Form, FormGroup, Button, Input,FormFeedback} from 'reactstrap'
-import {useHistory} from 'react-router-dom'
+import React, { useState } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+  Media,
+  Form,
+  FormGroup,
+  Button,
+  Input,
+  FormFeedback
+} from "reactstrap";
+import { useHistory } from "react-router-dom";
 
 const SignUp = props => {
   const { buttonLabel, className } = props;
@@ -10,8 +22,8 @@ const SignUp = props => {
   const [usernameValid /*, setusernameValid*/] = useState(true);
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [formReady, setFormReady] = useState(false)
-  const history = useHistory()
+  const [formReady, setFormReady] = useState(false);
+  const history = useHistory();
 
   const handleInput = e => {
     if (e.target.name === "name") {
@@ -27,123 +39,137 @@ const SignUp = props => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    props.signUpUser({
-      name: name,
-      username: username,
-      password: password,
-      email: email
-    }, success=>{
-      if(success){
-        alert('loggin success')
+    props.signUpUser(
+      {
+        name: name,
+        username: username,
+        password: password,
+        email: email
+      },
+      success => {
+        if (success) {
+          alert("loggin success");
+        }
       }
-    })
+    );
   };
 
-  return(
+  return (
     <>
-    <Container className='middle-screen dark body-pattern'>
-      <Row className='no-margin no-padding'>
-        <Col>
-          <Card className='rounded-corner dark-form small-20px'>
-          <Row>
-            <Col className='logo'>
-            </Col>
-          </Row>
-            <CardBody>
-            <Form onSubmit={handleSubmit}>
-              <FormGroup>
-                <Input
-                  type="text"
-                  name="name"
-                  placeholder="Full Name"
-                  value={name}
-                  onChange={handleInput}
-                  {...(name.length >= 6
-                    ? usernameValid
-                      ? { valid: true }
-                      : { invalid: true }
-                    : name.length > 0
-                    ? { invalid: true }
-                    : "")}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Input
-                  type="text"
-                  name="username"
-                  placeholder="Username (min 6 characters)"
-                  value={username}
-                  onChange={e => {
-                    handleInput(e);
-                  }}
-                  {...(username.length >= 6
-                    ? usernameValid
-                      ? { valid: true }
-                      : { invalid: true }
-                    : username.length > 0
-                    ? { invalid: true }
-                    : "")}
-                />
-                <FormFeedback
-                  {...(username.length > 0 && username.length >= 6
-                    ? usernameValid
-                      ? { valid: true }
-                      : { invalid: "true" }
-                    : { invalid: "true" })}
+      <Container className="middle-screen dark body-pattern">
+        <Row className="no-margin no-padding">
+          <Col>
+            <Card className="rounded-corner dark-form small-20px">
+              <Row>
+                <Col className="logo"></Col>
+              </Row>
+              <CardBody>
+                <Form onSubmit={handleSubmit}>
+                  <FormGroup>
+                    <Input
+                      type="text"
+                      name="name"
+                      placeholder="Full Name"
+                      value={name}
+                      onChange={handleInput}
+                      {...(name.length >= 6
+                        ? usernameValid
+                          ? { valid: true }
+                          : { invalid: true }
+                        : name.length > 0
+                        ? { invalid: true }
+                        : "")}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Input
+                      type="text"
+                      name="username"
+                      placeholder="Username (min 6 characters)"
+                      value={username}
+                      onChange={e => {
+                        handleInput(e);
+                      }}
+                      {...(username.length >= 6
+                        ? usernameValid
+                          ? { valid: true }
+                          : { invalid: true }
+                        : username.length > 0
+                        ? { invalid: true }
+                        : "")}
+                    />
+                    <FormFeedback
+                      {...(username.length > 0 && username.length >= 6
+                        ? usernameValid
+                          ? { valid: true }
+                          : { invalid: "true" }
+                        : { invalid: "true" })}
+                    >
+                      {username.length >= 6
+                        ? usernameValid
+                          ? "Sweet, this username is available!"
+                          : "Sorry, this username is taken!"
+                        : "Must be minimum 6 characters"}
+                    </FormFeedback>
+                  </FormGroup>
+
+                  <FormGroup>
+                    <Input
+                      type="password"
+                      name="password"
+                      placeholder="Password (min 6 character)"
+                      value={password}
+                      onChange={handleInput}
+                      {...(password.length >= 6
+                        ? { valid: true }
+                        : password.length > 0
+                        ? { invalid: true }
+                        : "")}
+                    />
+                  </FormGroup>
+
+                  <FormGroup>
+                    <Input
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={handleInput}
+                      {...(email.length === 0
+                        ? ""
+                        : validateEmail(email)
+                        ? { valid: true }
+                        : { invalid: true })}
+                    />
+                  </FormGroup>
+                  <Button
+                    disabled={
+                      name && username && email && password ? false : true
+                    }
+                    className="dark-btn-full-width"
+                    color="info"
+                  >
+                    {" "}
+                    Create{" "}
+                  </Button>
+                </Form>
+                <Button
+                  onClick={() => history.push("/signin")}
+                  color="link"
+                  className="center"
                 >
-                  {username.length >= 6
-                    ? usernameValid
-                      ? "Sweet, this username is available!"
-                      : "Sorry, this username is taken!"
-                    : "Must be minimum 6 characters"}
-                </FormFeedback>
-              </FormGroup>
-
-              <FormGroup>
-                <Input
-                  type="password"
-                  name="password"
-                  placeholder="Password (min 6 character)"
-                  value={password}
-                  onChange={handleInput}
-                  {...(password.length >= 6
-                    ? { valid: true }
-                    : password.length > 0
-                    ? { invalid: true }
-                    : "")}
-                />
-              </FormGroup>
-
-              <FormGroup>
-                <Input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={handleInput}
-                  {...(password.length >= 6
-                    ? { valid: true }
-                    : validateEmail(email)
-                    ? { invalid: true }
-                    : "")}
-                />
-              
-              </FormGroup>
-              <Button disabled={name && username && email && password ? false : true} className='dark-btn-full-width' color='info'> Create </Button>
-            </Form>
-              <Button onClick={()=>history.push('/signin')} color='link' className="center">
                   Have an account? Sign in here
                 </Button>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
 
 const validateEmail = email => {
   var re = /^(([^<>()[\]\\.,;:\s@0"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(\.))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
